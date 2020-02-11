4b8abe28a9b1c01a1d3c62560a10cf9d4a85a12f
Kira Yang <changru.yang02@gmail.com>
2020-02-11 10:20:45 +0800

    install eslint and reset style

---
+++ src/containers/LogIn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LogIn() {
+  return (
+    <div>
+      LogIn
+    </div>
+  )
+}
+
---
+++ src/style/global.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+//variable
+export const mainColor ="#499BA2"
+
+export const GlobalStyle = createGlobalStyle`
+  html{
+    body{
+      margin: 0;
+      padding: 0
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+
+    li {
+      margin: 0;
+    }
+
+    a {
+      text-decoration: none;
+    }
+  }
+`
